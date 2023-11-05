96be869218c5faaa1f30e0a5fc4d589ce7c13ce9
Siarhei Biadniuk <sergeybednyuk@gmail.com>
2023-11-05 16:34:11 +0100

    Add examples for Single respinsibility principle

---
+++ SingleResponsibilityPrinciple/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class User	
+{
+	private HttpClient _client;
+
+	private int _id;
+
+	public string UserName {  get; set; }
+
+	public string Password { get; set; }
+
+	public User(string userName, string password)
+	{
+		_client = new HttpClient();
+
+		var q = new Random();
+		_id = q.Next();
+		UserName = userName;
+		Password = password;
+	}
+
+
+    #region BadPractice
+    public string Log()
+	{
+		return ($"Log {UserName} User");
+	}
+
+	public async Task<string?> SendHttpRequestAsync(string url, HttpMethod method, string content)
+	{
+		try
+		{
+			var request = new HttpRequestMessage(method, url);
+
+			if (string.IsNullOrEmpty(content))
+			{
+				request.Content = new StringContent(content, System.Text.Encoding.UTF8, "application/json");
+			}
+
+			var response = await _client.SendAsync(request);
+
+			if (response.IsSuccessStatusCode) 
+			{
+				return await response.Content.ReadAsStringAsync();
+			}
+			else
+			{
+				Console.WriteLine($"HTTP resuest faild with status code: {response.StatusCode}");
+				return null;
+			}
+		}
+		catch (HttpRequestException ex)
+		{
+			Console.WriteLine(ex.Message);
+			return null;
+		}
+	}
+
+	public	void SomeMethodSaveData()
+	{
+		//SomeCode
+	}
+
+    #endregion
+}
---
+++ SingleResponsibilityPrinciple/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class UserRepository
+{
+	public UserRepository()
+	{
+	}
+
+
+	public void Save(User user)
+	{
+		Console.WriteLine($"User {user.UserName} was saved to DB");
+	}
+}
