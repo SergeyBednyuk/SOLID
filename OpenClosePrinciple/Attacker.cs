28f3c5c94240fadc649e95c85f9f37a925f60258
Siarhei Biadniuk <sergeybednyuk@gmail.com>
2023-11-07 20:01:06 +0100

    Add Open-Close principle example

---
+++ OpenClosePrinciple/Attacker.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public interface Attacker
+{
+    void Attack();
+}
---
+++ OpenClosePrinciple/Sword.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Sword : Weapon
+{
+    public Sword(int damage, int range)
+        :base(type: "sword", damage, range)
+    {
+            
+    }
+
+    public override void Attack()
+    {
+        Console.WriteLine($"attack of sword deal {Damage} damage at {Range} range");
+    }
+}
