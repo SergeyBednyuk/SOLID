b8e74e9ffa8af2c8622ee3e7ce65de65baf03e49
Siarhei Biadniuk <sergeybednyuk@gmail.com>
2023-11-07 20:55:11 +0100

    Add Liskov Substitution principle example

---
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LiskovSubstitutionPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StartApp(new MySqlDataBase());
+            StartApp(new MongoDataBase());
+
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        public static void StartApp(DataBase dataBase)
+        {
+            dataBase.Connect();
+        }
+    }
+}
---
+++ LiskovSubstitutionPrinciple/RelationDataBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class RelationDataBase : DataBase
+{
+	public RelationDataBase()
+	{
+	}
+
+    public override void Connect()
+    {
+        base.Connect();
+    }
+
+    public override void Disconnect()
+    {
+        base.Disconnect();
+    }
+
+    public virtual void JoinTabled(string tableAName, string tableBName) { }
+}
